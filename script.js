6454579e97e6fefe49191c782bdf264345bbe324
Anthony Malary <amalary@bu.edu>
2023-04-11 15:17:27 -0700

    ChatBot firt response

---
+++ script.js
@@ -0,0 +1,18 @@
+import { config } from "dotenv";
+config()
+
+
+import {Configuration, OpenAIApi} from "openai";
+
+const openai = new OpenAIApi(new Configuration({
+  apiKey:process.env.API_KEY
+}))
+
+console.log(openai)
+
+openai.createChatCompletion({
+  model: "gpt-3.5-turbo",
+  messages: [{role:"user", content: 'Hello ChatGPT'}]
+}).then(res =>{
+  console.log(res.data.choices)
+})
